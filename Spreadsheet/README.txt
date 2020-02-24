c8618becdb60abab0ef76816d7d475d707fafe40
Jacob Morrison <iamjacobmorrison@gmail.com>
2020-02-24 16:37:36 -0700

    copied first changes from framework, implemented labels for selected cell, cell value and contents, also tried to implement moving selected cell with arrow keys to no avail

--- Spreadsheet/README.txt
+++ Spreadsheet/README.txt
@@ -1,3 +1,63 @@
+Authors:     Jacob Morrison & James Gibb
+Partner:    None
+Date:       February 28, 2020
+Course:     CS 3500, University of Utah, School of Computing
+Assignment: Assignment #6 - Spreadsheet Front-End Graphical User Interface
+Copyright:  CS 3500 and [Jacob Morrison] - This work may not be copied for use in Academic Coursework.
+
+1. Comments to Evaluators:
+
+    
+
+2. Assignment Specific Topics:
+
+   
+
+3. Consulted Peers:
+
+    
+
+4. References:
+
+    1. 
+
+5. Examples of Good Software Practice:
+
+
+6. GitHub Repo and Commit Number
+
+    - *** TODO *** update Please include a short section near the top of the README that indicates the github repository URL as well as the commit number associated with 
+    the work you handin to GradeScope.
+
+7. Partnership Information
+
+    - *** TODO *** The name of both you and your partner should of course appear on any code you create, as well as at the top of the README document. 
+    Create a short section in the README called Partnership which briefly describes the contributions of each member.  If all code was completed via pair 
+    programming, state as much.  If each partner completed (some small) different sections of work, also indicated this (see Branching below). 
+
+8. Branching
+    
+    - *** TODO *** Detail any branches that were made so that each partner could work on separate functionality.  Describe the merge process and any challenges involved in it.  
+    Make sure to list the commit numbers. 
+
+9. Additional Features and Design Decisions
+ 
+    - *** TODO *** In clear prose, describe to the TAs the list of design choices you made as well as your added features.  This should include how to utilize the features.
+
+10. Best (Team) Practices
+
+    -*** TODO *** If you worked in a partnership, write two paragraphs.  The first should list one or two examples where the partnership was most effective.  What did you do 
+    right and how did it make the coding process faster, and the code better? Include details on how assigning tasks helped. The second paragraph should be about an area of 
+    teamwork that you need to improve upon.
+
+6. Estimated Time: PS5 -  18 hours     
+7. Actual Time:    PS5 -                      
+    Learning:                       
+    Programming:          
+    Debugging:              
+
+------------------------------------------------------------------------------------------------------
+
 Author:     Jacob Morrison
 Partner:    None
 Date:       February 14, 2020
---
+++ Spreadsheet/SpreadsheetGUI/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿using SpreadsheetGrid_Framework;
+using System.Windows.Forms;
+
+namespace SpreadsheetGrid_Core
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainControlArea = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutForSelectedCellValueContents = new System.Windows.Forms.TableLayoutPanel();
+            this.CellValueTextBox = new System.Windows.Forms.TextBox();
+            this.CellValueLabel = new System.Windows.Forms.Label();
+            this.SelectedCellLabel = new System.Windows.Forms.Label();
+            this.CellContentsTextBox = new System.Windows.Forms.TextBox();
+            this.SelectedCellTextBox = new System.Windows.Forms.TextBox();
+            this.CellContentsLabel = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.grid_widget = new SpreadsheetGrid_Framework.SpreadsheetGridWidget();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip.SuspendLayout();
+            this.MainControlArea.SuspendLayout();
+            this.tableLayoutForSelectedCellValueContents.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1684, 40);
+            this.menuStrip.TabIndex = 1;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.closeToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(72, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.NewToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.CloseToolStripMenuItem_Click);
+            // 
+            // MainControlArea
+            // 
+            this.MainControlArea.AutoSize = true;
+            this.MainControlArea.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.MainControlArea.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.MainControlArea.Controls.Add(this.tableLayoutForSelectedCellValueContents);
+            this.MainControlArea.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.MainControlArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainControlArea.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.MainControlArea.Location = new System.Drawing.Point(6, 6);
+            this.MainControlArea.Margin = new System.Windows.Forms.Padding(6);
+            this.MainControlArea.MinimumSize = new System.Drawing.Size(200, 192);
+            this.MainControlArea.Name = "MainControlArea";
+            this.MainControlArea.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.MainControlArea.Size = new System.Drawing.Size(1672, 192);
+            this.MainControlArea.TabIndex = 4;
+            this.MainControlArea.Paint += new System.Windows.Forms.PaintEventHandler(this.MainControlArea_Paint);
+            // 
+            // tableLayoutForSelectedCellValueContents
+            // 
+            this.tableLayoutForSelectedCellValueContents.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.InsetDouble;
+            this.tableLayoutForSelectedCellValueContents.ColumnCount = 2;
+            this.tableLayoutForSelectedCellValueContents.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15.85624F));
+            this.tableLayoutForSelectedCellValueContents.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 84.14376F));
+            this.tableLayoutForSelectedCellValueContents.Controls.Add(this.CellValueTextBox, 1, 1);
+            this.tableLayoutForSelectedCellValueContents.Controls.Add(this.CellValueLabel, 0, 1);
+            this.tableLayoutForSelectedCellValueContents.Controls.Add(this.SelectedCellLabel, 0, 0);
+            this.tableLayoutForSelectedCellValueContents.Controls.Add(this.CellContentsTextBox, 1, 2);
+            this.tableLayoutForSelectedCellValueContents.Controls.Add(this.SelectedCellTextBox, 1, 0);
+            this.tableLayoutForSelectedCellValueContents.Controls.Add(this.CellContentsLabel, 0, 2);
+            this.tableLayoutForSelectedCellValueContents.Location = new System.Drawing.Point(3, 55);
+            this.tableLayoutForSelectedCellValueContents.Name = "tableLayoutForSelectedCellValueContents";
+            this.tableLayoutForSelectedCellValueContents.RowCount = 3;
+            this.tableLayoutForSelectedCellValueContents.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutForSelectedCellValueContents.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutForSelectedCellValueContents.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutForSelectedCellValueContents.Size = new System.Drawing.Size(946, 134);
+            this.tableLayoutForSelectedCellValueContents.TabIndex = 0;
+            // 
+            // CellValueTextBox
+            // 
+            this.CellValueTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.CellValueTextBox.Location = new System.Drawing.Point(157, 52);
+            this.CellValueTextBox.Name = "CellValueTextBox";
+            this.CellValueTextBox.Size = new System.Drawing.Size(250, 31);
+            this.CellValueTextBox.TabIndex = 0;
+            this.CellValueTextBox.TextChanged += new System.EventHandler(this.CellValueTextBox_TextChanged);
+            // 
+            // CellValueLabel
+            // 
+            this.CellValueLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.CellValueLabel.AutoSize = true;
+            this.CellValueLabel.Location = new System.Drawing.Point(38, 55);
+            this.CellValueLabel.Name = "CellValueLabel";
+            this.CellValueLabel.Size = new System.Drawing.Size(110, 25);
+            this.CellValueLabel.TabIndex = 1;
+            this.CellValueLabel.Text = "Cell Value";
+            this.CellValueLabel.Click += new System.EventHandler(this.CellValueLabel_Click);
+            // 
+            // SelectedCellLabel
+            // 
+            this.SelectedCellLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.SelectedCellLabel.AutoSize = true;
+            this.SelectedCellLabel.Location = new System.Drawing.Point(9, 11);
+            this.SelectedCellLabel.Name = "SelectedCellLabel";
+            this.SelectedCellLabel.Size = new System.Drawing.Size(139, 25);
+            this.SelectedCellLabel.TabIndex = 2;
+            this.SelectedCellLabel.Text = "Selected Cell";
+            this.SelectedCellLabel.Click += new System.EventHandler(this.SelectedCellLabel_Click_1);
+            // 
+            // CellContentsTextBox
+            // 
+            this.CellContentsTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.CellContentsTextBox.Location = new System.Drawing.Point(157, 95);
+            this.CellContentsTextBox.Name = "CellContentsTextBox";
+            this.CellContentsTextBox.Size = new System.Drawing.Size(750, 31);
+            this.CellContentsTextBox.TabIndex = 4;
+            this.CellContentsTextBox.TextChanged += new System.EventHandler(this.CellContentsTextBox_TextChanged);
+            // 
+            // SelectedCellTextBox
+            // 
+            this.SelectedCellTextBox.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.SelectedCellTextBox.Location = new System.Drawing.Point(157, 8);
+            this.SelectedCellTextBox.Name = "SelectedCellTextBox";
+            this.SelectedCellTextBox.Size = new System.Drawing.Size(250, 31);
+            this.SelectedCellTextBox.TabIndex = 5;
+            this.SelectedCellTextBox.TextChanged += new System.EventHandler(this.SelectedCellTextBox_TextChanged);
+            // 
+            // CellContentsLabel
+            // 
+            this.CellContentsLabel.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.CellContentsLabel.AutoSize = true;
+            this.CellContentsLabel.Location = new System.Drawing.Point(50, 98);
+            this.CellContentsLabel.Name = "CellContentsLabel";
+            this.CellContentsLabel.Size = new System.Drawing.Size(98, 25);
+            this.CellContentsLabel.TabIndex = 3;
+            this.CellContentsLabel.Text = "Contents";
+            this.CellContentsLabel.Click += new System.EventHandler(this.CellContentsLabel_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSize = true;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.MainControlArea, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.grid_widget, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 40);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 192F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1684, 960);
+            this.tableLayoutPanel1.TabIndex = 6;
+            // 
+            // grid_widget
+            // 
+            this.grid_widget.AutoSize = true;
+            this.grid_widget.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.grid_widget.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid_widget.Location = new System.Drawing.Point(6, 198);
+            this.grid_widget.Margin = new System.Windows.Forms.Padding(6);
+            this.grid_widget.MaximumSize = new System.Drawing.Size(4200, 3846);
+            this.grid_widget.Name = "grid_widget";
+            this.grid_widget.Size = new System.Drawing.Size(1672, 756);
+            this.grid_widget.TabIndex = 0;
+            this.grid_widget.Load += new System.EventHandler(this.grid_widget_Load);
+            this.grid_widget.KeyDown += new System.Windows.Forms.KeyEventHandler(this.grid_widget_KeyDown);
+            this.grid_widget.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.grid_widget_KeyPress);
+            this.grid_widget.KeyUp += new System.Windows.Forms.KeyEventHandler(this.grid_widget_KeyUp);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(359, 44);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // SimpleSpreadsheetGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1684, 1000);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "SimpleSpreadsheetGUI";
+            this.Text = "Sample GUI - Copy/Modify/Profit";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SimpleSpreadsheetGUI_KeyPress);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.MainControlArea.ResumeLayout(false);
+            this.tableLayoutForSelectedCellValueContents.ResumeLayout(false);
+            this.tableLayoutForSelectedCellValueContents.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        private SpreadsheetGridWidget grid_widget;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+
+        private FlowLayoutPanel MainControlArea;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutForSelectedCellValueContents;
+        private TextBox CellValueTextBox;
+        private Label CellValueLabel;
+        private Label SelectedCellLabel;
+        private Label CellContentsLabel;
+        private TextBox CellContentsTextBox;
+        private TextBox SelectedCellTextBox;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+
+        #endregion
+    }
+}
+
